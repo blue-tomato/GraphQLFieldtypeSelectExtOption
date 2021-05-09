e27f0e523bdf5ca581e9c63c459cd222f5a22172
Markus Tiefenbacher <markus.tiefenbacher@gmail.com>
2021-05-09 21:06:05 +0200

    This module should only be read only

--- GraphQLFieldtypeSelectExtOption.module.php
+++ GraphQLFieldtypeSelectExtOption.module.php
@@ -1,47 +1,167 @@
-<?php namespace ProcessWire\GraphQL\Type\Fieldtype;
+<?php
 
+namespace ProcessWire;
+
+use GraphQL\Type\Definition\Type;
+use GraphQL\Type\Definition\ObjectType;
+use GraphQL\Type\Definition\InputObjectType;
 use ProcessWire\GraphQL\Cache;
-use ProcessWire\GraphQL\Type\Fieldtype\Traits\FieldTrait;
+use ProcessWire\Page;
+use ProcessWire\Field;
+use ProcessWire\GraphQL\Utils;
 
-class FieldtypeThirdParty
+class GraphQLFieldtypeSelectExtOption extends WireData implements Module
 {
-  use FieldTrait;
 
-  public static function &type($field)
+  public static function getModuleInfo()
   {
-    $thirdPartyClassName = self::getThirdPartyClassName($field);
-    $type =& Cache::type("type--{$field->name}", function () use ($field, $thirdPartyClassName) {
-      return $thirdPartyClassName::getType($field);
-    });
-    return $type;
+
+    return array(
+      'title' => 'GraphQLFieldtypeSelectExtOption',
+      'version' => '1.3.0',
+      'summary' => 'GraphQL support for FieldtypeSelectExtOption.',
+      'href' => 'https://github.com/blue-tomato/GraphQLFieldtypeSelectExtOption',
+      'requires' => ['ProcessGraphQL', 'FieldtypeSelectExtOption']
+    );
   }
 
-  public static function &inputField($field)
+  public static $name = 'SelectExtOption';
+  public static $inputName = 'SelectExtOptionInput';
+
+  private static function _translateNativeType($orig)
   {
-    $inputType =& Cache::field("input--{$field->name}", function () use ($field) {
-      $fieldSettings = self::field($field);
-      $thirdPartyClassName = self::getThirdPartyClassName($field);
-      if (method_exists($thirdPartyClassName, 'getInputType')) {
-        $fieldSettings['type'] = $thirdPartyClassName::getInputType($field);
+    $trans = array(
+      'VAR_STRING' => Type::string(),
+      'STRING' => Type::string(),
+      'BLOB' => Type::string(),
+      'LONGLONG' => Type::int(),
+      'LONG' => Type::int(),
+      'SHORT' => Type::int(),
+      'DATETIME' => Type::string(),
+      'DATE' => Type::string(),
+      'DOUBLE' => Type::float(),
+      'TIMESTAMP' => Type::int(),
+      'FLOAT' => Type::float(),
+      'BIT' => Type::boolean()
+    );
+    return $trans[$orig] ?? Type::string(); //fallback string
+  }
+
+  private static function _getSelectExtOptFields(Field $field)
+  {
+    $db = wire('database');
+    $dbTable = $field->data['option_table'];
+    $result = $db->query("SELECT * FROM `$dbTable` LIMIT 0");
+    return $result;
+  }
+
+  public static function getType(Field $field)
+  {
+    $mainType = null;
+
+    $fields = (array) [];
+    $result = self::_getSelectExtOptFields($field);
+    $columnCount = $result->columnCount();
+    for ($i = 0; $i < $columnCount; $i++) {
+      $type = null;
+
+      $column = $result->getColumnMeta($i);
+
+      $name = (string) $column['name'];
+      $type = self::_translateNativeType($column['native_type']);
+
+      if ($name && !empty($name)) {
+        $fields[$name] = [
+          'type' => $type,
+          'description' => "The $name column field of the SelectExtOption table",
+          'resolve' => function ($value) use ($name) {
+            return $value[$name];
+          }
+        ];
       }
-      return $fieldSettings;
-    });
-    return $inputType;
-  }
-
-  public static function setValue($page, $field, $value)
-  {
-    $thirdPartyClassName = self::getThirdPartyClassName($field);
-    return $thirdPartyClassName::setValue($page, $field, $value);
-  }
-
-  public static function getThirdPartyClassName($field)
-  {
-    $thirdPartyClassName = "\\ProcessWire\\GraphQL" . $field->type->className();
-    if (class_exists($thirdPartyClassName)) {
-      return $thirdPartyClassName;
-    } else {
-      return null;
     }
+
+    $mainType = Cache::type(self::getName($field), function () use ($fields, $field) {
+      return new ObjectType([
+        'name' => self::getName($field),
+        'fields' => $fields
+      ]);
+    });
+
+    if (self::isMultiple($field)) {
+      return Type::listOf($mainType);
+    }
+
+    return $mainType;
+  }
+
+
+  // todo: check how to remove getInputType?
+  public static function getInputType(Field $field)
+  {
+    $mainInputType = null;
+
+    $fields = (array) [];
+    $result = self::_getSelectExtOptFields($field);
+    $columnCount = $result->columnCount();
+    for ($i = 0; $i < $columnCount; $i++) {
+      $type = null;
+
+      $column = $result->getColumnMeta($i);
+
+      $name = (string) $column['name'];
+      $type = self::_translateNativeType($column['native_type']);
+
+      if ($name && !empty($name)) {
+        $fields[$name] = $type;
+      }
+    }
+
+    $mainInputType = Cache::type(self::getInputName($field), function () use ($fields, $field) {
+      return new InputObjectType([
+        'name' => self::getInputName($field),
+        'fields' => $fields
+      ]);
+    });
+
+    if (self::isMultiple($field)) {
+      return Type::listOf($mainInputType);
+    }
+
+    return $mainInputType;
+  }
+
+  /*
+  // this module is read only
+  public static function setValue(Page $page, Field $field, $value)
+  {
+    $fieldName = $field->name;
+    $result = self::_getSelectExtOptFields($field);
+    $columnCount = $result->columnCount();
+    for ($i = 0; $i < $columnCount; $i++) {
+      $column = $result->getColumnMeta($i);
+      $name = (string) $column['name'];
+      $page->$fieldName->$name = $value[$name];
+    }
+  }
+  */
+
+  private static function getName(Field $field)
+  {
+    return Utils::normalizeTypeName("{$field->name}" . self::$name);
+  }
+
+  private static function getInputName(Field $field)
+  {
+    return Utils::normalizeTypeName("{$field->name}" . self::$inputName);
+  }
+
+  public static function isMultiple(Field $field = null)
+  {
+    if ($field && in_array($field->input_type, ["InputfieldSelectMultiple", "InputfieldCheckboxes", "InputfieldAsmSelect"])) {
+      return true;
+    }
+
+    return false;
   }
 }
